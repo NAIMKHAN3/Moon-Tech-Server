aaac9d57b143f49a8805dac1caa967aff62ddad2
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-07 13:25:23 +0600

    Server Create

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send({ status: true, Message: 'Moon Tech Server is Running' })
+})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujhfrio.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const productsCollection = client.db("moontech").collection("products")
+
+        app.get('/products', async (req, res) => {
+            const result = await productsCollection.find({}).toArray();
+            console.log(result)
+            res.send(result)
+
+        })
+    }
+    catch {
+        res.send({ status: false, Message: "Server Is Down" })
+    }
+
+}
+run().catch(e => console.log(e))
+
+
+
+app.listen(port, () => {
+    console.log("Moon Tech Server is running")
+})
